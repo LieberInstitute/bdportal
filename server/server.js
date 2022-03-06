72f6dff1836a51da7c129353824a1bc0d01625b3
Geo Pertea <geo.pertea@gmail.com>
2022-03-05 21:36:49 -0500

    added conditional request forwarding to /auth and /email routes

--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const axios = require('axios')
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
 
 const { Pool, Client } = require("pg");
 //const http = require('http');
@@ -12,11 +15,38 @@ const config = require('./config.json');
 
 //ATTN: make sure config.json has these properly set and localized
 // r_filedir must match the R_FILE_STAGING directory (or a local mounted directory of it)
-const dbserver = config['dbserver']; 
-const dbuser = config['dbuser'];
-const dbpasswd = config['dbpasswd'];
 const dbname = config['dbname'];
-const r_filedir= config['r_filedir'];
+const app_port = config['app_port'];
+
+const hostname = process.env.HOSTNAME;
+require('dotenv').config();
+const dbuser = process.env.DB_USER;
+const dbpasswd = process.env.DB_PASS;
+let auth_srv = 'http://192.168.77.16:6443'; //where to send the auth request, "/auth" will be appended
+let dbserver = process.env.DB_SRV;
+let r_filedir='/dbdata/cdb/r_staging'; //default on srv16
+let mail_url = 'http://192.168.77.14:1244/';
+if (hostname=="gryzen" || hostname=="gi7" || hostname=="gdebsrv") {
+    if (!dbserver) dbserver='gdebsrv';
+    if (hostname=="gryzen")
+             r_filedir="\\\\gdebsrv\\ssdata\\postgresql\\r_staging\\";
+       else if (hostname=="gi7") r_filedir="/data/gdebsrv_ssdata/postgresql/r_staging";
+           else r_filedir="/ssdata/postgresql/r_staging";
+    mail_url = 'http://gdebsrv:14244/';
+    auth_srv= 'http://192.168.2.2:16443';
+} else { //LIBD devel or server, or aws  
+  if (!dbserver) dbserver='localhost';
+  if (hostname=="linwks34") {
+         r_filedir=(dbserver=='localhost') ? '/ssdata/postgresql/r_staging' : '';
+  } //else { //MUST be on srv16 itself, or aws 
+    //r_filedir='/dbdata/cdb/r_staging';
+    //if (hostname!=='srv16') console.log("WARNING: srv16 assumed, r_filedir set to: ", r_filedir)    
+  //}
+}
+const auth_url = `${auth_srv}/auth`;
+console.log(`dbserver=${dbserver} (${r_filedir}), mail url: ${mail_url}`)
+const jwt_shh =  process.env.JWTSHH
+
 /* -- config as in Bill's timesheet server
 const jwtsecret = config['jwtsecret'];
 const authserver = config['authserver'];
@@ -41,8 +71,10 @@ const credentials = {
 const fs = require('fs');
 const path = require('path');
 const db = require('./db');
+const e = require('express');
+const { emitWarning } = require('process');
+
 const app = express();
-const port = 4000;
 
 db.init(credentials);
 
@@ -71,6 +103,10 @@ function poolTest() {
   });
   
 }
+// ----------- middleware setup -----
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Allow CORS requests
 app.use(  (req, res, next) => {
@@ -79,10 +115,65 @@ app.use(  (req, res, next) => {
     next();
 });
 
+// ----------- routes handling
+
 app.get('/', (req, res) => {
     res.status(400).send('Invalid Request');
 });
 
+app.post('/auth', (req, res) => {
+  //Bill's request was also passing ca : fs.readFileSync(cafile) as request param
+  /* for axios, certificate can be passed as a  httpsAgent parameter
+     which can be read in advance, before the routing:
+
+    const agent = new https.Agent({ ca: fs.readFileSync("./resource/bundle.crt"),  
+         rejectUnauthorized: (process.env.NODE_ENV !== 'development')});
+
+    and then add an additional axios.post param :
+      axios.post(url, body, 
+        { httpsAgent : agent } ).then( ... )
+  */
+  axios.post(auth_url, {
+    username: req.body.username,
+    password: req.body.password,
+  }).then(authres=> {
+    //{signed_user:, token:}
+    console.log("response data:", authres.data)
+    res.status(200).json(authres.data);
+    //log authentication here
+    //updateLog(db, req.body.username, 'AUTH')
+  }).catch((err) => {
+    res.status(500).json({ message: err });
+    //updateLog(db, req.body.username, 'AUTHFAIL')
+  });
+});
+
+/* once authentication took place, authres.data.token will be stored
+ by the client and sent as req.token (or req.authorization) with every
+ future restricted/sensitive request, so the request should be validated 
+ like this:
+app.post('/status', (req, res)=> {
+    const jwt_token = req.headers.authorization;
+    let username = req.body.username; // does not matter
+    try {
+      // override username with the one in the encoded token
+      const decoded_token = jwt.verify(jwt_token, jwt_shh);
+      username = decoded_token; //override prev given username
+    } catch(err) {
+      res.status(403).send("EMPLOYEE: Unauthorized Access /employee AUTH");
+      return;
+    }
+   ...
+  });
+*/
+
+app.post('/email', (req, res) => {
+  axios.post(mail_url, req.body).then(ares=> {
+    res.status(200).json(ares.data); 
+  }).catch((err) => {
+    res.status(500).json({ message: err });
+  });
+});
 //const mdata={};
 //const jzfile='rnaseq_samples.json.gz';
 //-- load json.gz file
@@ -169,7 +260,7 @@ async function queryDx(res) {
 
 // ----  route with sub-routes/terms
 app.get(['/pgdb/:qry/:dtype','/pgdb/:qry'] , (req, res)=> {
-    console.log("got pg query:"+req.params.qry);
+    console.log(`got pg query: ${req.params.qry}`);
     //possible subpages: sel (default), ex, rep
     switch (req.params.qry) {
       case 'dsets': queryDatasets(res, req.params.dtype); 
@@ -198,7 +289,7 @@ app.get('/ruthere', (req, res)=> {
 
 app.get('/pgplrinit', (req, res)=> {
   //DEBUG: disable this for now:
-  res.send('nop');return;
+  res.send('nop'); return;
   db.query('select * from r_version()', [], (err, dbrows)=>{
     if (err) {
       //res.status(500).send({ error: err.severity+': '+err.code, message: err.message })
@@ -213,4 +304,4 @@ app.get('/pgplrinit', (req, res)=> {
 
 
 //adding some fake dummy sub-tabs for the RNASeq entry
-app.listen(port, () => console.log(`Server started on port ${port}`))
+app.listen(app_port, () => console.log(`Server listening on port ${app_port}`))
