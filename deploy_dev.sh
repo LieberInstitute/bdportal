98c4bad68ff0012a61a5ae9e42b78a53a30904bd
Geo Pertea <geo.pertea@gmail.com>
2022-07-20 17:17:33 -0400

    deploy_dev fixed

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -1,20 +1,27 @@
 #/bin/bash
 ##should update .env first
 lasthash=$(git rev-parse --short HEAD)
-lastdate=$(git log -1 --format='%ai' | gawk '{print $1}')
-sed -i -E "s/COMMIT_DATE=.+/COMMIT_DATE=${lastdate}/" .env
-sed -i -E "s/COMMIT_HASH=.+/COMMIT_HASH=${lasthash}/" .env
-host=$(hostname -d)
+lastdate=$(git log -1 --format='%ai' | awk '{print $1}')
+sed -i -E "s/COMMIT_DATE=.*/COMMIT_DATE=${lastdate}/" .env
+sed -i -E "s/COMMIT_HASH=.*/COMMIT_HASH=${lasthash}/" .env
+host=$(hostname -s)
 rdest="" #remote target (home)
 docroot=/var/www/html
+mwsrv=geowks.lieber.local
 case "$host" in
   "gryzen" | "gi7" | "guvb")
       docroot=/data/nginx/html
+      mwsrv="gdebsrv"
       rdest="gdebsrv" ;;
    "srv16")
-      sed -i -E "s/_MWSERVER=.+/_MWSERVER=http://srv16.lieber.local:4095/" .env
+      mwsrv="srv16.lieber.local"
+      ;;
+   "linwks34")
+      mwsrv="geowks.lieber.local"
       ;;
 esac
+echo "mwsrv = $mwsrv"
+sed -i -E "s|_MWSERVER=.*|_MWSERVER=http://${mwsrv}:4095|" .env
 /bin/rm -rf dist/*
 npm run build-dev-based
 
