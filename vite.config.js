3686d90791b8c849d08b03a5aaebf18715b157f5
Geo Pertea <geo.pertea@gmail.com>
2023-01-14 11:27:27 -0500

    added option/default to dev with mw server on gwrk-wg

--- vite.config.js
+++ vite.config.js
@@ -7,19 +7,25 @@ process.env.VITE_HOST=host;
 let auth_proxy='http://gdebsrv:16600'
 let mail_proxy='http://gdebsrv:16244'
 let pgdb_proxy='http://localhost:4095'
+let remote_h2gw=1 //remote middleware develpment at home through wireguard
 
 export default defineConfig(({ command, mode }) => {
   const devmode=(mode=='development')
   let nodemw_srv= devmode ? 'http://localhost:4095' : 'http://gdebsrv:4095';
-  if (host.match(/^linwks/) || host.match(/^srv/)) {
+  if (host.match(/^linwks/) || host.match(/^srv/) || remote_h2gw ) {
     //dev at LIBD:
      auth_proxy='http://192.168.77.16:16600'
      mail_proxy='http://192.168.77.16:1244'
-     //pgdb_proxy='http://192.168.77.16:4095'
-     pgdb_proxy='http://10.17.9.59:4095'
+     pgdb_proxy='http://192.168.77.16:4095'
+     //pgdb_proxy='http://10.17.9.59:4095'
      //if
      //nodemw_srv=devmode ? 'http://localhost:4095' : 'http://192.168.77.16:4095'
      nodemw_srv=devmode ? 'http://localhost:4095' :  'http://10.17.9.59:4095'
+     if (devmode && remote_h2gw) {
+        nodemw_srv='http://192.168.77.3:4095'
+        console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nWARNING: dev mode points to middleware on", nodemw_srv)
+        console.log("  if you want to run that mw locally, set remote_h2gw=0 in vite.config.js")
+     }
   }
 
   process.env.VITE_NODESRV=nodemw_srv;
